69880e36cec9e3c9ec0cb3cff0912c6684824fd3
le vinh phu <vinhphuvtv2@gmail.com>
2022-01-20 01:55:50 +0700

    Update Form Toi

--- Nhom5_QuanLySieuThi/BUS/BUS_KhachHang.cs
+++ Nhom5_QuanLySieuThi/BUS/BUS_KhachHang.cs
@@ -1,9 +1,11 @@
 ﻿using Nhom5_QuanLySieuThi.DAO;
 using System;
 using System.Collections.Generic;
+using System.Data.Entity.Infrastructure;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace Nhom5_QuanLySieuThi.BUS
 {
@@ -14,6 +16,18 @@ namespace Nhom5_QuanLySieuThi.BUS
         public Customer GetCustomer(string phoneNumber)
         {
             return dAO_KhachHang.GetCustomer(phoneNumber);
+        }
+
+        public void suaThongTinKhachHang(Customer c)
+        {
+            try
+            {
+                dAO_KhachHang.suaThongTinKhachHang(c);
+            }
+            catch (DbUpdateException ex) // bat loi lien quan den database (rang buoc, ...)
+            {
+                MessageBox.Show(ex.Message);
+            }
         }
     }
 }
--- Nhom5_QuanLySieuThi/Properties/Resources.Designer.cs
+++ Nhom5_QuanLySieuThi/Properties/Resources.Designer.cs
@@ -133,6 +133,16 @@ namespace Nhom5_QuanLySieuThi.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap images {
+            get {
+                object obj = ResourceManager.GetObject("images", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap search {
             get {
                 object obj = ResourceManager.GetObject("search", resourceCulture);
